a3ad4d86c4a46bae830cb452637639a4d72a560d
Miles Biringanine <milesgabriel23@gmail.com>
2022-08-14 00:09:19 -0500

    Added display functions connected to form inputs

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
-// An array for storing read list
+"use strict";
 
-let library = [];
+let container = document.querySelector(".container");
+let form = document.getElementById("form-id");
+let button = document.querySelector(".addBtn");
 
-function addSelection(book) {
-  return library.push(book);
-}
+let myLibrary = [];
 
-// Constructor function
-
-function Book(title, author, numberOfPages) {
+function Book(title, author, pages) {
   this.title = title;
   this.author = author;
-  this.numberOfPages = numberOfPages;
-  return addSelection(title);
+  this.pages = pages;
 }
 
-let book1 = new Book("Kingdom", "Yasuhisa Hara", 1500);
-let book2 = new Book("Berserk", "Kentaro Miura", 1700);
-let book3 = new Book("Attack on Titan", "Hajime Isayama", 1400);
+// Create book and add to library
 
-// A function that loops through the array and displays each book on the page
+function createBook(title, author, pages) {
+  title = document.getElementById("title").value;
+  author = document.getElementById("author").value;
+  pages = document.getElementById("pages").value;
+  let newBook = new Book(title, author, pages);
 
-function displayBookList() {
-  library.forEach((book) => {
-    document.body.innerHTML += book + ", ";
+  myLibrary.push(newBook);
+}
+
+// Display books
+
+function displayBook() {
+  let newBook = document.createElement("div");
+  let edit = document.createElement("button");
+  let remove = document.createElement("button");
+  let btnContainer = document.createElement("div");
+  edit.textContent = "Edit";
+  remove.textContent = "Remove";
+  btnContainer.appendChild(edit);
+  btnContainer.appendChild(remove);
+
+  newBook.innerHTML = `Title: ${
+    myLibrary[myLibrary.length - 1].title
+  } </br> Author: ${myLibrary[myLibrary.length - 1].author} </br> Pages: ${
+    myLibrary[myLibrary.length - 1].pages
+  }`;
+
+  newBook.appendChild(btnContainer);
+
+  container.appendChild(newBook);
+
+  remove.addEventListener("click", (e) => {
+    myLibrary.pop();
   });
 }
 
-displayBookList();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createBook();
+  displayBook();
+  form.reset();
+});
+
+// function to remove card
