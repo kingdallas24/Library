63341df585ab6e187d51c1e6510891db9629127a
Miles Biringanine <milesgabriel23@gmail.com>
2022-08-15 16:38:11 -0500

    Updated UI and added read status to form

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ let form = document.getElementById("form");
 let titleInput = document.getElementById("titleInput");
 let pagesInput = document.getElementById("pagesInput");
 let authorInput = document.getElementById("authorInput");
+let readInput = document.getElementById("readInput");
 let titleMsg = document.getElementById("titleMsg");
 let authorMsg = document.getElementById("authorMsg");
 let pagesMsg = document.getElementById("pagesMsg");
 let books = document.getElementById("books");
 let add = document.getElementById("add");
+let changeRead = document.querySelector(".changeRead");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -33,11 +35,21 @@ let formValidation = () => {
 let myLibrary = [];
 
 let acceptData = () => {
+  checkReadStatus();
   myLibrary.push({
     title: titleInput.value,
     author: authorInput.value,
     pages: pagesInput.value,
+    read: readInput.value,
   });
+
+  function checkReadStatus() {
+    if (readInput.checked == true) {
+      readInput.value = "Read";
+    } else {
+      readInput.value = " Not Read";
+    }
+  }
 
   localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
   console.log(myLibrary);
@@ -48,10 +60,13 @@ let createBook = () => {
   books.innerHTML = "";
   myLibrary.map((x, y) => {
     return (books.innerHTML += ` <div id = ${y}>
-    <span class="fw-bold">Title: ${x.title}</span>
-    <span >Author: ${x.author}</span>
-    <span>Number of Pages: ${x.pages}</span>
+    <span class="fw-bold">${x.title}</span>
+    <span >${x.author}</span>
+    <span>${x.pages}</span>
+    <span>${x.read}</span>
+
     <span class="options">
+   
       <i onClick = "editBook(this)"   data-bs-toggle="modal"
       data-bs-target="#form"class="fa-solid fa-pen-to-square"></i>
       <i onClick = "deleteBook(this); createBook()" class="fa-solid fa-trash"></i>
@@ -64,9 +79,9 @@ let createBook = () => {
 
 let deleteBook = (e) => {
   e.parentElement.parentElement.remove();
-  myLibrary.splice(e.parentElement.parentElement.id, 1);
-
   console.log(e.parentElement.parentElement.id);
+  myLibrary.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
 };
 
 let editBook = (e) => {
@@ -74,6 +89,7 @@ let editBook = (e) => {
   titleInput.value = selectedBook.children[0].innerHTML;
   authorInput.value = selectedBook.children[1].innerHTML;
   pagesInput.value = selectedBook.children[2].innerHTML;
+
   deleteBook(e);
 };
 
